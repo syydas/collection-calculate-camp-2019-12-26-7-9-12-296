067a2b332f68b07c54f525d13125ea9a3c3fa551
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:33:02 +0800

    [zhengsuqin]feat: complete choose_no_common_elements

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -3,6 +3,13 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var res = [];
+  collection_a.forEach(function(item) {
+    if (-1 === collection_b.indexOf(item)) {
+      res.push(item);
+    }
+  });
+  return res;
 }
 
 module.exports = choose_no_common_elements;
