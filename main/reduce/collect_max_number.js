4b5e888e4dcacd64e223717643f403269c07cfc9
Asuu <suqinzheng_0624@163.com>
2019-12-30 22:41:58 +0800

    [zhengsuqin]feat: complete collect_max_number

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -2,6 +2,13 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  var max = collection[0];
+  collection.forEach(function(item) {
+    if (item > max) {
+      max = item;
+    }
+  });
+  return max;
 }
 
 module.exports = collect_max_number;
