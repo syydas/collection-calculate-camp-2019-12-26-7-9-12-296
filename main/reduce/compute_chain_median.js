369520cd527850c1074260bb44c7d638dd17b989
Asuu <suqinzheng_0624@163.com>
2019-12-30 23:05:04 +0800

    [zhengsuqin]feat: complete compute_chain_median

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,25 @@
-'use strict';
+"use strict";
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  var median = 0;
+  var tmpArr = collection.split("->");
+  var arr = [];
+  for (var item of tmpArr) {
+    arr.push(parseInt(item));
+  }
+  arr = arr.sort(function(a, b) {
+    return a - b;
+  })
+  if (arr.length % 2 !== 0) {
+    var index = parseInt(arr.length / 2);
+    median = arr[index];
+  }
+  else {
+    var index = arr.length / 2;
+    median = (arr[index - 1] + arr[index]) / 2;
+  }
+  return median;
 }
 
 module.exports = compute_chain_median;
