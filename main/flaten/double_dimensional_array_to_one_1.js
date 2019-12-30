29a0befdc32ed36eb3062c9bd5a4487aa26d0315
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:49:29 +0800

    [zhengsuqin]feat: complete double_to_one

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,19 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var res = [];
+  collection.forEach(function(item) {
+    if (item instanceof Array) {
+      item.forEach(function(itemIn) {
+        res.push(itemIn);
+      })
+    }
+    else {
+      res.push(item);
+    }
+  });
+  return res;
+  //return (collection + '').split(',').map(x => Number(x));
 }
 
 module.exports = double_to_one;
