ca61b4a79b00c69ea1945f9c4c738700f5fc8907
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:36:52 +0800

    [zhengsuqin]feat: complete choose_divisible_integer

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -3,6 +3,15 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var res = [];
+  collection_a.forEach(function(item_a) {
+    collection_b.forEach(function(item_b) {
+      if (0 === item_a % item_b) {
+        res.push(item_a);
+      }
+    })
+  });
+  return res;
 }
 
 module.exports = choose_divisible_integer;
