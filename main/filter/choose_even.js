b6098405bd9adf0792dd29ddd8ae1884422a449e
Asuu <suqinzheng_0624@163.com>
2019-12-30 20:48:59 +0800

    [zhengsuqin] feat: complete choose_even and choose_multiples_of_three

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -3,6 +3,13 @@
 function choose_even(collection) {
 
   //在这里写入代码
+  var res = [];
+  collection.forEach(function(item) {
+    if (0 === item % 2) {
+      res.push(item);
+    }    
+  });
+  return res;
 }
 
 module.exports = choose_even;
--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -3,6 +3,13 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
+  var res = [];
+  collection.forEach(function(item) {
+    if (0 === item % 3) {
+      res.push(item);
+    }
+  });
+  return res;
 }
 
 module.exports = choose_multiples_of_three;
