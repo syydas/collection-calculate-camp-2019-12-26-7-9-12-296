1dd8080529aa39e37b0231ac6868fdb7c425f83e
Asuu <suqinzheng_0624@163.com>
2019-12-30 20:11:47 +0800

    [zhengsuqin] feat: complete get_intersection_spec

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -2,6 +2,15 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
-}
+  var res = [];
+  collection_b.forEach(item_b => {
+    collection_a.forEach(item_a => {
+      if (item_a === item_b) {
+        res.push(item_b);
+      }
+    })
+  });
+  return res;
+} 
 
 module.exports = get_intersection;
