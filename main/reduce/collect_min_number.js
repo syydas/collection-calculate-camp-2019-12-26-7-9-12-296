bc54ec65f5b04896f20b040f113f67936a548e1d
Asuu <suqinzheng_0624@163.com>
2019-12-30 22:43:06 +0800

    [zhengsuqin]feat: complete collect_min_number

--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -2,6 +2,13 @@
 
 function collect_min_number(collection) {
   //在这里写入代码
+  var min = collection[0];
+  collection.forEach(function(item) {
+    if (item < min) {
+      max = item;
+    }
+  });
+  return min;
 }
 
 module.exports = collect_min_number;
