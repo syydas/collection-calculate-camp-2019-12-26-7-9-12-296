c9545fe9b9e234d1ad2006013f270bbba51d6239
Asuu <suqinzheng_0624@163.com>
2019-12-30 19:39:05 +0800

    [zhengsuqin] feat: complete collect_all_even

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -2,6 +2,14 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var res = [];
+  collection.forEach(function(item) {
+    if (0 === item % 2) {
+      res.push(item);
+    }
+  });
+  return res;
 }
 
 module.exports = collect_all_even;
+
