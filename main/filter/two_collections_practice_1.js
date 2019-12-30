7a32bb494b70e48b708d153d3ccf3e0c0879ceb0
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:28:20 +0800

    [zhengsuqin]feat: complete two_collections_practice_1

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -3,6 +3,15 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var res = [];
+  collection_a.forEach(item_a => {
+    collection_b.forEach(item_b => {
+      if (item_a === item_b) {
+        res.push(item_a);
+      }
+    });
+  });
+  return res;
 }
 
 module.exports = choose_common_elements;
