2a373184511737c26e3cdb680fa256065eeaf246
Asuu <suqinzheng_0624@163.com>
2019-12-30 22:28:06 +0800

    [zhengsuqin]feat: complete number_map_to_word_over_26

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,16 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  var res = [];
+  res = collection.map(item => {
+    var flag = parseInt(item / 27);
+    if (flag === 0) {
+      return String.fromCharCode(item + 96);
+    } else {
+      var letter = String.fromCharCode((item % 26) + 96);
+      return String.fromCharCode(flag + 96).concat(letter);
+    }
+  })
+  return res;
 };
 
 module.exports = number_map_to_word_over_26;
