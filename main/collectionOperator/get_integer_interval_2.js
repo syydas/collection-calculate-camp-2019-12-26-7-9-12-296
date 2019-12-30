ffe6298e1a6129862567069bb16b8f2311896265
Asuu <suqinzheng_0624@163.com>
2019-12-30 19:51:09 +0800

    [zhengsuqin] feat: complete get_integer_interval_2

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,34 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  var res = [];
+  if (number_b > number_a) {
+    var i = 0;
+    while (number_b >= number_a) {
+      if (0 === number_a % 2) {
+        res[i] = number_a;
+        i++;
+        number_a++;
+      } else {
+        number_a++;
+      }
+    }
+  } else if (number_a > number_b) {
+    var i = 0;
+    while (number_a >= number_b) {
+      if (0 === number_a % 2) {
+        res[i] = number_a;
+        i++;
+        number_a--;
+      } else {
+        number_a--;
+      }
+    }
+  } else if (number_b == number_a && number_a % 2 == 0) {
+    var i = 0;
+    res[i] = number_a;
+  }
+  return res;  
 }
 
 module.exports = get_integer_interval_2;
