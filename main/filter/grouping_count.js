4078cc572744c7d854342fd6f8e3cbae13bc6341
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:24:21 +0800

    [zhengsuqin]feat: complete grouping_count

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -3,6 +3,19 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  var res = [];
+  collection.sort();
+  for (var i = 0; i < collection.length;) {
+    var count = 0;
+    for (var j = i; j < collection.length; j++) {
+      if (collection[i] === collection[j]) {
+        count++;
+      }
+    }
+    res[collection[i]] = count;
+    i += count;
+  }
+  return res;
 }
 
 module.exports = grouping_count;
