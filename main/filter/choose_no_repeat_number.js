6bfbca41f2fdc2ad525696161afcc081de1a03aa
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:17:54 +0800

    [zhengsuqin]feat: complete choose_no_repeat_number

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  var res = [collection[0]];
+  for (var i = 1; i < collection.length; i++) {
+    if (i === collection.indexOf(collection[i])) {
+      res.push(collection[i]);
+    }
+  }
+  return res;
 }
 
 module.exports = choose_no_repeat_number;
