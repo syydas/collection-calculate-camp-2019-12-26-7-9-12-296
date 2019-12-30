915a060f1650f872d028c82ae64c98da7d50b6ad
Asuu <suqinzheng_0624@163.com>
2019-12-30 22:58:05 +0800

    [zhengsuqin]feat: complete compute_average

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -2,6 +2,15 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  /*var sum = collection.reduce((num1, num2) => {
+    return num1 + num2;
+  });*/
+  var sum = 0;
+  for (var i in collection) {
+    sum += collection[i];
+  }
+  var average = sum / collection.length;
+  return average;
 }
 
 module.exports = compute_average;
