dc37dc602c573d6b99e1715d0fdab8c634fd8c37
Asuu <suqinzheng_0624@163.com>
2019-12-30 21:57:13 +0800

    [zhengsuqin]feat: complete double_to_one_2

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,15 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var res = [];
+  collection.forEach(item => {
+    item.forEach(itemIn => {
+      if (-1 === res.indexOf(itemIn)) {
+        res.push(itemIn);
+      }
+    })
+  });
+  return res;
 }
 
 module.exports = double_to_one;
