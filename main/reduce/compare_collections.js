faaf237754d046337b3c6e311fa705f9c3efd19c
Asuu <suqinzheng_0624@163.com>
2019-12-30 22:48:18 +0800

    [zhengsuqin]feat: complete compare_collections

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -2,6 +2,17 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  if (collection_a.length === collection_b.length) {
+    for (var i = 0; i < collection_b.length; i++) {
+      if (collection_a[i] === collection_b[i])
+      return true;
+    }
+  }
+  else {
+    return false;
+  }
+  
+  //return collection_a.sort().toString() === collection_b.sort().toString();
 }
 
 module.exports = compare_collections;
