2c572ba0d299c540bfca9e43989c7914a2c6bc15
Asuu <suqinzheng_0624@163.com>
2019-12-30 20:43:00 +0800

    [zhengsuqin] feat: complete get_union

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -2,6 +2,14 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  var i = 0;
+  while (i < collection_b.length) {
+    if (-1 === collection_a.indexOf(collection_b[i])) {
+      collection_a.push(collection_b[i]);
+    }
+    i++;
+  }
+  return collection_a;
 }
 
 module.exports = get_union;
