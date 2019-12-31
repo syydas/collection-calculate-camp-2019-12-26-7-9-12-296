47231032279196bbdc925e22155ec6c2ae32f11b
Asuu <suqinzheng_0624@163.com>
2019-12-31 18:23:00 +0800

    [zhengsuqin]feat: complete create_updated_collection_full_three_minus_one

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,5 +1,14 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var collectionB = object_b.value;
+  for (var itemA of collection_a) {
+    for (var itemB of collectionB) {
+      if (itemA.key === itemB) {
+        itemA.count -= parseInt(itemA.count / 3);
+      }
+    }
+  }
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
