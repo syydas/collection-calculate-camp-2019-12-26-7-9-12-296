20332e05f8c05895f9546d0a5702799bb198f7ca
Asuu <suqinzheng_0624@163.com>
2019-12-31 17:13:08 +0800

    [zhengsuqin]feat: complete find_last_even

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -2,6 +2,13 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var res = 0;
+  for (var item of collection) {
+    if (0 === item % 2) {
+      res = item;
+    }
+  }
+  return res;
 }
 
 module.exports = find_last_even;
