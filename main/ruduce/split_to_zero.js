4f31bd5e33c5b39a410ae93fc9420bc10d0c82cf
Asuu <suqinzheng_0624@163.com>
2019-12-31 18:46:07 +0800

    [zhengsuqin]feat: complete split_to_zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,14 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var res = [];
+  var item = number;
+  res.push(item);
+  do {
+    item = Number((item - interval).toFixed(1));
+    res.push(item);
+  } while (item > 0);
+  return res;
 }
 
 module.exports = spilt_to_zero;
