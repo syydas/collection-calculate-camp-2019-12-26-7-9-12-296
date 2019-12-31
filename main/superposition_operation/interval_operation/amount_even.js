4df3af872c216b83754c8eee6af51bf013c7f990
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:13:03 +0800

    [zhengsuqin]feat: complete amount_even

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 function amount_even(collection) {
-
   //在这里写入代码
+  var sum = 0;
+  for (var item of collection) {
+    if (0 === item % 2) {
+      sum += item;
+    }
+  }
+  return sum;
 }
 
 module.exports = amount_even;
