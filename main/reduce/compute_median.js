6aa1e94218cbd78461714279159f09f2bac82d5f
Asuu <suqinzheng_0624@163.com>
2019-12-31 17:02:11 +0800

    [zhengsuqin]feat: complete compute_median

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -2,6 +2,19 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  var median = 0;
+  var index = 0;
+  collection = collection.sort(function(a, b) {
+    return a - b;
+  });
+  if (collection.length % 2 != 0) {
+    index = parseInt(collection.length / 2);
+    median = collection[index];
+  } else {
+    index = collection.length / 2;
+    median = (collection[index - 1] + collection[index]) / 2;
+  }
+  return median;
 }
 
 module.exports = compute_median;
