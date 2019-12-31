842a5db5c5087eb84bc777570b346cd923a0ce36
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:52:28 +0800

    [zhengsuqin]feat: complete is_exist_element

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,8 @@
-'use strict';
-var is_exist_element = function(collection,element){
-
+"use strict";
+var is_exist_element = function(collection, element) {
+  var res = collection.filter(function(value, index) {
+    return 0 === index % 2; 
+  });
+  return -1 !== res.indexOf(element);
 };
 module.exports = is_exist_element;
