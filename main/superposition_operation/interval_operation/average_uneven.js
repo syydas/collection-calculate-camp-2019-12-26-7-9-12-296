4d535a389d45ec6af3e7e3e905551b0f2d0f841f
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:15:43 +0800

    [zhengsuqin]feat: complete average_uneven

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 function average_uneven(collection) {
-
   //在这里写入代码
+  var sum = 0;
+  var count = 0;
+  for (var item of collection) {
+    if (0 !== item % 2) {
+      sum += item;
+      count++;
+    }
+  }
+  var average = sum / count;
+  return average;
 }
 
 module.exports = average_uneven;
