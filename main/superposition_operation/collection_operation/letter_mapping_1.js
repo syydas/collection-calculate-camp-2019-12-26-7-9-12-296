a93d43fc92a480bc77f46464f5e70ac00c0d4dbf
Asuu <suqinzheng_0624@163.com>
2019-12-31 18:56:19 +0800

    [zhengsuqin]feat: complete even_to_letter

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,11 @@
-'use strict';
+"use strict";
 
 function even_to_letter(collection) {
-
   //在这里写入代码
+  var tempArr = collection.filter(function(item) {
+    return item % 2 === 0;
+  });
+  return tempArr.map(item => String.fromCharCode(item + 96));
 }
 
 module.exports = even_to_letter;
