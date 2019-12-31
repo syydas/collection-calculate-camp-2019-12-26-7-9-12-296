4ea27c2e8eb28fe8dedb3065c5c8d651cac118ef
Asuu <suqinzheng_0624@163.com>
2019-12-31 20:53:53 +0800

    [zhengsuqin]feat: complete even_asc_odd_desc

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,26 @@
-'use strict';
-var even_asc_odd_desc = function(collection){
-
+"use strict";
+var even_asc_odd_desc = function(collection) {
+  var odd = [];
+  var even = [];
+  var res = [];
+  for (var item of collection) {
+    if (item % 2 == 0) {
+      even.push(item);
+    } else {
+      odd.push(item);
+    }
+  }
+  res = [
+    even.sort(function(a, b) {
+      return a - b;
+    }),
+    odd.sort(function(a, b) {
+        return b - a;
+      })
+  ];
+  res = res.reduce(function(a, b) {
+    return a.concat(b);
+  });
+  return res;
 };
 module.exports = even_asc_odd_desc;
