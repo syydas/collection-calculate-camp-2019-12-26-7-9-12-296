59f014fdccdf881ad07b8b9ac39a64b00866c595
Asuu <suqinzheng_0624@163.com>
2019-12-31 17:29:11 +0800

    [zhengsuqin]feat: complete collect_same_elements_key_value

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,19 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var arrA = [];
+  for (var itemA of collection_a) {
+    arrA.push(itemA.key);
+  }
+  var arrB = object_b.value;
+  var res = [];
+  for (var itemA of arrA) {
+    for (var itemB of arrB) {
+      if (itemA === itemB) {
+        res.push(itemA);
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = collect_same_elements;
