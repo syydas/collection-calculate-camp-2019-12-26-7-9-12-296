393252160cc0abfeb6e9109d2a99209c459c70c8
Asuu <suqinzheng_0624@163.com>
2019-12-31 18:15:46 +0800

    [zhengsuqin]feat: complete collect_same_elements_name_summary

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,19 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var res = [];
+  var obj = {};
+  collection.forEach(element => {
+    var ele = element.split(/[:\-\[\]]/);
+    if (obj[ele[0]] == null) {
+      obj[ele[0]] = ele.length === 1 ? 1 : parseInt(ele[1]);
+    } else {
+      obj[ele[0]] += ele.length === 1 ? 1 : parseInt(ele[1]);
+    }
+  });
+  for (var item in obj) {
+    res.push({ name: item, summary: obj[item] });
+  } 
+  return res;
 }
 
 module.exports = count_same_elements;
