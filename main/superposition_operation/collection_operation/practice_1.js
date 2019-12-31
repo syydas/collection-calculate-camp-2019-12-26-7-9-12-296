904bb1926b42969f627dc07e502fcfc0d7ed6ec5
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:05:31 +0800

    [zhengsuqin]feat: complete hybrid_operation

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -3,6 +3,11 @@
 function hybrid_operation(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  for (var item of collection) {
+    sum += item * 3 + 2;
+  }
+  return sum;
 }
 
 module.exports = hybrid_operation;
