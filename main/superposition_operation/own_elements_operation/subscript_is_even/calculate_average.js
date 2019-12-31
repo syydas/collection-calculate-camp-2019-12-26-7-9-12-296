bf3d6305a02e232914533b8c71590272c83d76a5
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:35:46 +0800

    [zhengsuqin]feat: complete calculate_average

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,15 @@
-'use strict';
-var calculate_average = function(collection){
-
+"use strict";
+var calculate_average = function(collection) {
+  var sum = 0;
+  var count = 0;
+  for (var key = 1; key < collection.length; key++) {
+      if (0 === key % 2) {
+        sum += collection[key];
+        count++;
+      }
+  }
+  var average = sum / count;
+  return average;
 };
+
 module.exports = calculate_average;
