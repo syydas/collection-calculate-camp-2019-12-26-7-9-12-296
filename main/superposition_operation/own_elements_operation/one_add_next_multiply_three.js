d8dba3acf32f88c6be72b5d7889e1b0de2a86d92
Asuu <suqinzheng_0624@163.com>
2019-12-31 20:56:03 +0800

    [zhengsuqin]feat: complete one_add_next_multiply_three

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,10 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+  var res = [];
+  for (var i = 0; i < collection.length - 1; i++) {
+    var item = (collection[i] + collection[i + 1]) * 3;
+    res.push(item);
+  }
+  return res;
 }
 module.exports = one_add_next_multiply_three;
