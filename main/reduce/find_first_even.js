713473525d5d6f06ecdc803d038ee20f3f3c1677
Asuu <suqinzheng_0624@163.com>
2019-12-31 17:10:14 +0800

    [zhengsuqin]feat: complete find_first_even

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -2,6 +2,12 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  for (var item of collection) {
+    if (0 === item % 2) {
+      return item;
+      break;
+    }
+  }
 }
 
 module.exports = find_first_even;
