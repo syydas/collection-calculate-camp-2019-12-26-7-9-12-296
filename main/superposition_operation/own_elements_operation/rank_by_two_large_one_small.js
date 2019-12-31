69cd074312386519a46709f0eb6fc95d26c46850
Asuu <suqinzheng_0624@163.com>
2019-12-31 21:01:18 +0800

    [zhengsuqin]feat: complete rank_by_two_large_one_small_spec

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,15 @@
-'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+"use strict";
+function rank_by_two_large_one_small(collection) {
+  var sortCollection = collection.sort(function(x, y) {
+    return x - y;
+  });
+  var rankCollection = sortCollection.slice();
+  for (var i = 0; i < collection.length - 2; i += 3) {
+    rankCollection[i] = collection[i + 1];
+    rankCollection[i + 1] = collection[i + 2];
+    rankCollection[i + 2] = collection[i];
+  }
+  return rankCollection;
 }
+
 module.exports = rank_by_two_large_one_small;
