c954a76a30881903d0d9e06995da9908172b2693
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:21:50 +0800

    [zhengsuqin]feat: complete median_to_letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
 
 function median_to_letter(collection) {
-
   //在这里写入代码
+  var sum = 0;
+  for (var item of collection) {
+    sum += item;
+  }
+  var average = Math.ceil(sum / collection.length);
+  var flag = parseInt(average / 27);
+  if (flag === 0) {
+    return String.fromCharCode(average + 96);
+  } else {
+    var letter = String.fromCharCode((average % 26) + 96);
+    return String.fromCharCode(flag + 96).concat(letter);
+  }
 }
 
 module.exports = median_to_letter;
