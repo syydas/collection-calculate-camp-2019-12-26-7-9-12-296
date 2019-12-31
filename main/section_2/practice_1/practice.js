d9e61e132b759f5f0c8b1882989096deacbc9e56
Asuu <suqinzheng_0624@163.com>
2019-12-31 17:38:58 +0800

    [zhengsuqin]feat: complete collect_same_elements_key_count

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,19 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var res = [];
+  var helpArr = [];
+  for (var item of collection) {
+    if (-1 === helpArr.indexOf(item)) {
+      helpArr.push(item);
+    }
+  }
+  for (var item of helpArr) {
+    var countArr = collection.filter(function(sameItem) {
+      return sameItem === item;
+    });
+    res.push({'key': item, 'count': countArr.length});
+  }
+  return res;
 }
 
 module.exports = count_same_elements;
