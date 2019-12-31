f8f18a42c35c2265a0f5a1f068e437595df10de5
Asuu <suqinzheng_0624@163.com>
2019-12-31 20:47:27 +0800

    [zhengsuqin]feat: complete single_element

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,17 @@
-'use strict';
-var single_element = function(collection){
-
+"use strict";
+var single_element = function(collection) {
+  var tempArr = collection.filter(function(value, index) {
+    if (1 === index % 2) {
+      return value;
+    }
+  });
+  var res = [];
+  var count = [];
+  for (var item of tempArr) {
+    if (tempArr.indexOf(item) === tempArr.lastIndexOf(item)) {
+      res.push(item);
+    }
+  }
+  return res;
 };
 module.exports = single_element;
