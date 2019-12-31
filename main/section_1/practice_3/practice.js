880857b81957de8838a6e267298865b6c6e1e8bd
Asuu <suqinzheng_0624@163.com>
2019-12-31 17:25:29 +0800

    [zhengsuqin]feat: complete collect_same_elements_value

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var collection_b = object_b.value;
+   var res = [];
+   for (var itemA of collection_a) {
+     for (var itemB of collection_b) {
+       if (itemA === itemB) {
+         res.push(itemA);
+       }
+     }
+   }
+   return res;
 }
 
 module.exports = collect_same_elements;
