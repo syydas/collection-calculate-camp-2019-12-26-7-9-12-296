0e0185d039327e50d423701d0b16f80cd5b69e52
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:10:26 +0800

    [zhengsuqin]feat: complete hybrid_operation_to_uneven

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -3,6 +3,13 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  for (var item of collection) {
+    if (0 !== item % 2) {
+      sum += item * 3 + 5;
+    }
+  }
+  return sum;
 }
 
 module.exports = hybrid_operation_to_uneven;
