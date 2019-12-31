67e396251e9a1e3daad0bb21c289c46841a1f126
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:40:29 +0800

    [zhengsuqin]feat: complete calculate_median

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,17 @@
 'use strict';
 var calculate_median = function(collection){
-
+  var median = 0;
+  var index = 0;
+  collection = collection.sort(function(a, b) {
+    return a - b;
+  });
+  if (collection.length % 2 != 0) {
+    index = parseInt(collection.length / 2);
+    median = collection[index];
+  } else {
+    index = collection.length / 2;
+    median = (collection[index - 1] + collection[index]) / 2;
+  }
+  return Math.ceil(median);
 };
 module.exports = calculate_median;
