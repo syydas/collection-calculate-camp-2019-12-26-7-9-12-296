aaac3ab7720633ce0048492ca7f6cf1be84c94b3
Asuu <suqinzheng_0624@163.com>
2019-12-31 19:03:37 +0800

    [zhengsuqin]feat: complete average_to_letter

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -3,6 +3,12 @@
 function average_to_letter(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  for (var item of collection) {
+    sum += item;
+  }
+  var average = Math.ceil(sum / collection.length);
+  return String.fromCharCode(average +96);
 }
 
 module.exports = average_to_letter;
